7ca297a15fdafe3d0bc260bbf727807a8f0014be
KealG <894712026@qq.com>
2021-11-30 23:26:50 +0800

    Update to mathematical-surfaces 2/3

--- Assets/Tabs/1-Basics/BuildingAGraph/GraphAni.cs
+++ Assets/Tabs/1-Basics/BuildingAGraph/GraphAni.cs
@@ -13,20 +13,26 @@ public class GraphAni : MonoBehaviour
 
     Transform[] points;
 
-    [SerializeField, Range(0, 1)]
-    int function;
+    [SerializeField]
+    FunctionLibrary.FuncName function;
 
     private void Awake()
     {
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
         float step = 2f /resolution;
         Vector3 scale = Vector3.one * step;
         Vector3 postion = Vector3.zero;
 
-        for (int i = 0;  ++i< resolution;)
+        for (int i = 0, x = 0, z = 0;  i< points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = points[i] = Instantiate(pointerPrefab);
-            postion.x = (i + 0.5f) * step - 1;
+            postion.x = (x + 0.5f) * step - 1;
+            postion.z = (z + 0.5f) * step - 1;
             postion.y = postion.x * postion.x;
             point.localPosition = postion;            
             point.localScale = scale;
@@ -42,23 +48,25 @@ public class GraphAni : MonoBehaviour
     
     void Update()
     {
+        FunctionLibrary.m_FuncPtr f = FunctionLibrary.GetFunction(function);
         if (points != null && points.Length > 0)
         {
             for (int i = 0; i < points.Length; i++)
             {
                 var curPoint = points[i];
                 if (curPoint != null)
-                {
+                {                    
                     var curLocalPos = curPoint.localPosition;
-                    if (function == 0)
-                    {
-                        curLocalPos.y = FunctionLibrary.Wave(curLocalPos.x, Time.time);//Mathf.Sin(Mathf.PI * (curLocalPos.x + Time.time));//  * curLocalPos.x * curLocalPos.x;//Mathf.Pow(, 3f);
-                    }
-                    else
-                    {
-                        curLocalPos.y = FunctionLibrary.MultiWave(curLocalPos.x, Time.time);//Mathf.Sin(Mathf.PI * (curLocalPos.x + Time.time));//  * curLocalPos.x * curLocalPos.x;//Mathf.Pow(, 3f);
-                    }                    
+                    curLocalPos.y = f != null ? f(curLocalPos.x, curLocalPos.z, Time.time): curLocalPos.y;
                     curPoint.localPosition = curLocalPos;
+                    //if (function == 0)
+                    //{
+                    //    curLocalPos.y = FunctionLibrary.Wave(curLocalPos.x, Time.time);//Mathf.Sin(Mathf.PI * (curLocalPos.x + Time.time));//  * curLocalPos.x * curLocalPos.x;//Mathf.Pow(, 3f);
+                    //}
+                    //else
+                    //{
+                    //    curLocalPos.y = FunctionLibrary.MultiWave(curLocalPos.x, Time.time);//Mathf.Sin(Mathf.PI * (curLocalPos.x + Time.time));//  * curLocalPos.x * curLocalPos.x;//Mathf.Pow(, 3f);
+                    //}                                        
                 }
             }            
         }        
--- Assets/Tabs/1-Basics/Mathematical Surfaces/Function Library.cs
+++ Assets/Tabs/1-Basics/Mathematical Surfaces/Function Library.cs
@@ -6,16 +6,37 @@ namespace BasicChapter
 {
     public static class FunctionLibrary
     {
-        public static float Wave(float x, float t) 
+        public delegate float m_FuncPtr(float x, float z, float t);
+
+        public enum FuncName
         {
-            return Mathf.Sin(Mathf.PI * (x + t));
+            Wave,
+            MultiWave,
+            Ripple
+        }
+        static m_FuncPtr[] functions = { Wave, MultiWave, Ripple };
+        public static float Wave(float x, float z, float t) 
+        {
+            return Mathf.Sin(Mathf.PI * (x + z + t));
         }
 
-        public static float MultiWave(float x, float t)
+        public static float MultiWave(float x, float z, float t)
         {
             float y = Mathf.Sin(Mathf.PI * (x + 0.5f * t));
-            y += Mathf.Sin(2f * Mathf.PI * Mathf.Pow((x + t), 1)) * 0.5f;
+            y += Mathf.Sin(2f * Mathf.PI * Mathf.Pow((z + t), 1)) * 0.5f;
             return y * (2f / 3f);
+        }
+
+        public static float Ripple(float x, float z, float t)
+        {
+            float d = Mathf.Abs(x);
+            float y = Mathf.Sin(Mathf.PI * (4f * d - t));
+            return y / (1 + 10f / d);
+        }
+
+        public static m_FuncPtr GetFunction(FuncName name)
+        {            
+            return functions.Length > (int)name ? functions[(int)name] : null;
         }
 
     }
