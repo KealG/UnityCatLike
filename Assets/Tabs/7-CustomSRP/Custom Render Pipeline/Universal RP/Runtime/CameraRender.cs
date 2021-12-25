921c25e50d37b1d6e5b7f3105ccf3e8b75c721c7
KealG <894712026@qq.com>
2021-12-25 20:32:59 +0800

    URP自定义渲染管线初步尝试

---
+++ Assets/Tabs/7-CustomSRP/Custom Render Pipeline/Universal RP/Runtime/CameraRender.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRender
+{
+    ScriptableRenderContext context;
+    Camera camera;
+
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer(){ name = bufferName};
+    public void Render(ScriptableRenderContext _context, Camera _camera)
+    {
+        this.context = _context;
+        this.camera = _camera;
+
+        SetUp();
+        DrawVisibleGemetry();
+        Submit();
+    }
+
+    private void SetUp()
+    {
+        this.context.SetupCameraProperties(this.camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+
+        buffer.BeginSample(bufferName);        
+        ExecuteBuffer();        
+    }
+
+    private void DrawVisibleGemetry()
+    {
+        this.context.DrawSkybox(this.camera);
+    }
+
+    private void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        this.context.Submit();
+    }
+
+    private void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
