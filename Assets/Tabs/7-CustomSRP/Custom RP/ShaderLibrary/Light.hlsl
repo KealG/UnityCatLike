201376e46bce2dd63fff163f2d9d05c18359dc8b
KaiGe <kaigedong@outlook.com>
2022-11-16 22:05:23 +0800

    更新点光源与聚光灯章节
    -更新相应脚本以传输数据支持点光源与聚光灯

--- Assets/Tabs/7-CustomSRP/Custom RP/ShaderLibrary/Light.hlsl
+++ Assets/Tabs/7-CustomSRP/Custom RP/ShaderLibrary/Light.hlsl
@@ -2,6 +2,7 @@
 #define CUSTOM_LIGHT_INCLUDED
 
 #define MAX_DIRECTIONAL_LIGHT_COUNT 4
+#define MAX_OTHER_LIGHT_COUNT 64
 
 CBUFFER_START(_CustomLight)
 	int _DirectionalLightCount;
@@ -9,6 +10,12 @@ CBUFFER_START(_CustomLight)
 	float4 _DirectionalLightDirections[MAX_DIRECTIONAL_LIGHT_COUNT];
 	//x = lightshadow.strength y = cascadeCount * ShadowedDirectionalLightCount
 	float4 _DirectionalLightShadowData[MAX_DIRECTIONAL_LIGHT_COUNT];
+
+	int _OtherLightCount;
+	float4 _OtherLightColors[MAX_OTHER_LIGHT_COUNT];
+	float4 _OtherLightPositions[MAX_OTHER_LIGHT_COUNT];
+	float4 _OtherLightDirections[MAX_OTHER_LIGHT_COUNT];
+	float4 _OtherLightSpotAngles[MAX_OTHER_LIGHT_COUNT];
 CBUFFER_END
 
 struct Light {
@@ -19,6 +26,10 @@ struct Light {
 
 int GetDirectionalLightCount () {
 	return _DirectionalLightCount;
+}
+
+int GetOtherLightCount () {
+	return _OtherLightCount;
 }
 
 DirectionalShadowData GetDirectionalShadowData (int lightIndex, ShadowData shadowData) {
@@ -37,6 +48,22 @@ Light GetDirectionalLight (int index, Surface surfaceWS, ShadowData shadowData) 
 	light.direction = _DirectionalLightDirections[index].xyz;
 	DirectionalShadowData dirShadowData = GetDirectionalShadowData(index, shadowData);
 	light.attenuation = GetDirectionalShadowAttenuation(dirShadowData, shadowData, surfaceWS);	
+	return light;
+}
+
+Light GetOtherLight (int index, Surface surfaceWS, ShadowData shadowData) {
+	Light light;
+	light.color = _OtherLightColors[index].rgb;
+	float3 ray = _OtherLightPositions[index].xyz - surfaceWS.position;
+	light.direction = normalize(ray);
+	float distanceSqr = max(dot(ray, ray), 0.00001);
+	float rangeAttenuation = Square(saturate(1.0 - Square(distanceSqr * _OtherLightPositions[index].w)));
+	float4 spotAngles = _OtherLightSpotAngles[index];
+	float spotAttenuation = Square(
+		saturate(dot(_OtherLightDirections[index].xyz, light.direction) *
+		spotAngles.x + spotAngles.y)
+	);
+	light.attenuation = spotAttenuation * rangeAttenuation / distanceSqr;	
 	return light;
 }
 
