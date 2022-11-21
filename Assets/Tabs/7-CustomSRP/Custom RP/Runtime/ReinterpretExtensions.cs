2b6f465a9cd21043d21f50f91d999fcadfad7714
DESKTOP-JQ6NJ8Q\KaiGe <894712026@qq.com>
2022-11-21 15:59:01 +0800

    更新 多相机 章节
    -引入相机剔除层级与多相机画面重叠混合相关设置

--- Assets/Tabs/7-CustomSRP/Custom RP/Runtime/ReinterpretExtensions.cs
+++ Assets/Tabs/7-CustomSRP/Custom RP/Runtime/ReinterpretExtensions.cs
@@ -7,9 +7,10 @@ public static class ReinterpretExtensions
     [StructLayout(LayoutKind.Explicit)]
     struct IntFloat
     {
-
+        [FieldOffset(0)]
         public int intValue;
 
+        [FieldOffset(0)]
         public float floatValue;
     }
 
